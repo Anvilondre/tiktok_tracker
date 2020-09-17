676417a89c5b1e7ba0189679233c3fcf9525a25d
Roman Burakov <buracov.r@gmail.com>
2020-09-17 13:06:21 +0300

    Simple refactoring.

--- Tracker.py
+++ Tracker.py
@@ -3,7 +3,7 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 from TikTokAPI import TikTokAPI
-from account_id import account_id
+from data.account_id import account_id
 
 
 def do_something(sc, iterations=[], subs_history=[], likes_history=[]):
@@ -22,19 +22,19 @@ def do_something(sc, iterations=[], subs_history=[], likes_history=[]):
     plt.plot(iterations, subs_history)
     plt.ylabel('Subscribers')
     plt.xlabel('Time (minutes)')
-    plt.savefig('subs_fig.png')
+    plt.savefig('plots/subs_fig.png')
 
     # Likes
     plt.figure(1)
     plt.plot(iterations, likes_history)
     plt.ylabel('Likes')
     plt.xlabel('Time (minutes)')
-    plt.savefig('likes_fig.png')
+    plt.savefig('plots/likes_fig.png')
 
     # Save data
-    np.savetxt('likes.csv', likes_history, delimiter=',')
-    np.savetxt('subs.csv', subs_history, delimiter=',')
-    np.savetxt('iterations.csv', iterations, delimiter=',')
+    np.savetxt('data/likes.csv', likes_history, delimiter=',')
+    np.savetxt('data/subs.csv', subs_history, delimiter=',')
+    np.savetxt('data/iterations.csv', iterations, delimiter=',')
 
     s.enter(60, 1, do_something, (sc, iterations, subs_history, likes_history))
 
@@ -42,9 +42,9 @@ def do_something(sc, iterations=[], subs_history=[], likes_history=[]):
 if __name__ == '__main__':
     print('File loaded!')
     api = TikTokAPI()
-    iters = np.loadtxt('iterations.csv')
-    subs_hist = np.loadtxt('subs.csv')
-    likes_hist = np.loadtxt('likes.csv')
+    iters = np.loadtxt('data/iterations.csv')
+    subs_hist = np.loadtxt('data/subs.csv')
+    likes_hist = np.loadtxt('data/likes.csv')
     s = sched.scheduler(time.time, time.sleep)
     s.enter(0, 1, do_something, (s, iters, subs_hist, likes_hist))
     s.run()
